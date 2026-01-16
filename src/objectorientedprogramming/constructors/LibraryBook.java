1738287702af4d01b0591509eb934392efc4e228
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-01-16 22:50:39 +0530

    feat: add constructors module

---
+++ src/objectorientedprogramming/constructors/LibraryBook.java
@@ -0,0 +1,30 @@
+package objectorientedprogramming.constructors;
+class LibraryBook {
+
+    private String title;
+    private String author;
+    private double price;
+    private boolean available;
+
+    LibraryBook(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.available = true;
+    }
+
+    void borrowBook() {
+        if (available) {
+            available = false;
+            System.out.println("Book borrowed");
+        } else {
+            System.out.println("Not available");
+        }
+    }
+
+    public static void main(String[] args) {
+        LibraryBook b = new LibraryBook("Java", "James", 500);
+        b.borrowBook();
+        b.borrowBook();
+    }
+}

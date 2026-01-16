05688646ce7cdf4fe0d42458335d8f50f751d013
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-01-16 22:41:25 +0530

    feat: add core programming module

---
+++ src/coreprogramming/programmingelements/level1/Pens.java
@@ -0,0 +1,13 @@
+package coreprogramming.programmingelements.level1;
+
+public class Pens {
+    public static void main(String[] args) {
+        int pens = 14;
+        int numOfStudents = 3;
+        int penPerStudent = pens / numOfStudents;
+        int remainingPens = pens % numOfStudents;
+        System.out.println("The Pen Per Student is " + penPerStudent +
+                " and the remaining pen not distrubuted is " + remainingPens);
+    }
+
+}
